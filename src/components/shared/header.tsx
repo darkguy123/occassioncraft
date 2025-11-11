b9a500863b9fae8bdd818689d3937dbc6ea772e3
cycle breeze <cyclebreeze6@gmail.com>
2025-11-11 08:08:33 +0000

    add the glass morph effect and UI to all the strategic places of the web

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -40,7 +40,7 @@ export function Header() {
 
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full border-b bg-background/80 backdrop-blur-sm">
       <div className="container flex h-16 items-center">
         <div className="mr-4 flex">
            <Link href="/" className="mr-6 flex items-center space-x-2">
