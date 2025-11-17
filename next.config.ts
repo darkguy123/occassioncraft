f93ee743af45d16f58a632d9e9348e21848897cc
cycle breeze <cyclebreeze6@gmail.com>
2025-11-17 12:58:43 +0000

    all the images in the website are broken and not displaying in the front

--- next.config.ts
+++ next.config.ts
@@ -10,25 +10,10 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
+    domains: [
+      'placehold.co',
+      'firebasestorage.googleapis.com',
+      'picsum.photos',
     ],
   },
 };
