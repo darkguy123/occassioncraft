002fc615373b7ecb94fcf693d35659754ca0a0a9
cycle breeze <cyclebreeze6@gmail.com>
2025-12-03 10:58:46 +0000

    change the background color #336BFC and the font to light (_for element

--- src/components/shared/user-nav.tsx
+++ src/components/shared/user-nav.tsx
@@ -57,7 +57,7 @@ export function UserNav() {
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
             <p className="text-sm font-medium leading-none">{user?.displayName || 'User Name'}</p>
-            <p className="text-xs leading-none text-muted-foreground">{user?.email}</p>
+            <p className="text-xs leading-none text-white/80">{user?.email}</p>
           </div>
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
