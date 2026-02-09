8fdfe84671a88229d5568f0d2e2438685fd78a84
cycle breeze <cyclebreeze6@gmail.com>
2026-02-09 13:35:23 +0000

    lets check if this is the studio-8569439258-4b916 // Import the function

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,10 +2,9 @@
 export const firebaseConfig = {
   "projectId": "studio-8569439258-4b916",
   "appId": "1:916715795833:web:5ae718b877b2a81cc72ba8",
-  "storageBucket": "studio-8569439258-4b916.appspot.com",
+  "storageBucket": "studio-8569439258-4b916.firebasestorage.app",
   "apiKey": "AIzaSyBH_aj28sTtHWSUzm5Sc_qiulBjfCOzsgU",
   "authDomain": "studio-8569439258-4b916.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "916715795833"
 };
-
