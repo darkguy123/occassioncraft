6c8a4f57459a827ca4554b9e1fe2a192df33ef1d
darkguy123 <12982248+darkguy123@users.noreply.github.com>
2026-03-30 01:07:18 +0100

    chore: update storage bucket and permission rules

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@
 export const firebaseConfig = {
   "projectId": "studio-8569439258-4b916",
   "appId": "1:916715795833:web:5ae718b877b2a81cc72ba8",
-  "storageBucket": "studio-8569439258-4b916.appspot.com",
+  "storageBucket": "studio-8569439258-4b916",
   "apiKey": "AIzaSyBH_aj28sTtHWSUzm5Sc_qiulBjfCOzsgU",
   "authDomain": "studio-8569439258-4b916.firebaseapp.com",
   "measurementId": "G-5G3W9KTEH4",
