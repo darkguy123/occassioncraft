cfcd18a12ec93f61e31645e6c643606ee6452210
darkguy123 <12982248+darkguy123@users.noreply.github.com>
2026-06-25 12:16:33 +0100

    pull

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 
 export const firebaseConfig = {
-  "projectId": "studio-8569439258-4b916",
-  "appId": "1:916715795833:web:5ae718b877b2a81cc72ba8",
-  "storageBucket": "studio-8569439258-4b916.firebasestorage.app",
-  "apiKey": "AIzaSyBH_aj28sTtHWSUzm5Sc_qiulBjfCOzsgU",
-  "authDomain": "studio-8569439258-4b916.firebaseapp.com",
-  "measurementId": "G-5G3W9KTEH4",
-  "messagingSenderId": "916715795833"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-8569439258-4b916",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:916715795833:web:5ae718b877b2a81cc72ba8",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "studio-8569439258-4b916.firebasestorage.app",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyBH_aj28sTtHWSUzm5Sc_qiulBjfCOzsgU",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "studio-8569439258-4b916.firebaseapp.com",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-5G3W9KTEH4",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "916715795833"
 };
